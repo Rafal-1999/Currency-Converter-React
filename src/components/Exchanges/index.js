a5542c487748bae71a551838fa62d20d1c69c27b
Rafał <skapinka1999@wp.pl>
2022-10-22 20:13:17 +0200

    Created component Exchanges

---
+++ src/components/Exchanges/index.js
@@ -0,0 +1,10 @@
+const Exchanges = ({ title }) => (
+    <article className="currency-exchange__exchanges">
+        <h2 className="currency-exchange__article-title">
+            {title}
+        </h2>
+        <div className="currency-exchange__last-exchanges" />
+    </article>
+);
+
+export default Exchanges;
