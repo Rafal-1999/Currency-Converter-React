8019cbcb619c6869896417a95b4f0feb2d35db9a
Rafał <skapinka1999@wp.pl>
2022-10-29 19:34:36 +0200

    Created onFormSubmit function

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,12 +1,21 @@
 import "./style.css";
 
-const Form = ({ children, title }) => (
-    <form className="currency-exchange__form">
-        {children}
-        <button className="currency-exchange__submit">
-            {title}
-        </button>
-    </form>
-);
+const Form = ({ children, title }) => {
+    const onFormSubmit = (e) => {
+        e.preventDefault();
+    }
+
+    return (
+        <form
+            className="currency-exchange__form"
+            onSubmit={onFormSubmit}
+        >
+            {children}
+            <button className="currency-exchange__submit">
+                {title}
+            </button>
+        </form>
+    )
+};
 
 export default Form;
