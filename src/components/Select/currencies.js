9287dc27ce36b283c73da2f22ee4caa173da4bd9
Rafał <skapinka1999@wp.pl>
2022-12-11 12:46:55 +0100

    Updated rates and added new currencies

--- src/components/Select/currencies.js
+++ src/components/Select/currencies.js
@@ -1,7 +1,10 @@
 const currencies = [
-    { name: "PLN", value: 1 },
-    { name: "EUR", value: 4.57 },
-    { name: "USD", value: 3.87 }
+    { name: "PLN", value: 1.00 },
+    { name: "EUR", value: 4.69 },
+    { name: "USD", value: 4.45 },
+    { name: "HUF", value: 0.01 },
+    { name: "CZK", value: 0.19 },
+    { name: "GBP", value: 5.46 }
 ];
 
 export default currencies;
