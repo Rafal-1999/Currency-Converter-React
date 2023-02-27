bd6b500427144ed804fcaa48a70caa74419cd354
Rafał <skapinka1999@wp.pl>
2023-02-27 19:59:09 +0100

    Created styled component for Day

--- src/components/Day/index.js
+++ src/components/Day/index.js
@@ -1,16 +1,16 @@
 import { useCurrentDate } from "../../hooks/useCurrentDate";
-import "./style.css";
+import {DayContent} from "./styled";
 
 const Day = () => {
     const date = useCurrentDate();
 
     return (
-        <div className="day">
-            <p className="day__content">
+        <div>
+            <DayContent>
                 {date.toLocaleTimeString("pl",
                     { weekday: "long", day: "numeric", month: "long", year: undefined }
                 )}
-            </p>
+            </DayContent>
         </div>
     )
 };
---
+++ src/components/Day/styled.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const DayContent = styled.p`
+    margin-top: 0;
+    font-size: 20px;
+`;
