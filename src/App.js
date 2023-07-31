643ef854b7e7b82d1181dce6cf8f9fc8c0c7d560
Rafał <skapinka1999@wp.pl>
2023-07-31 20:36:18 +0200

    Trouble reading data

--- src/App.js
+++ src/App.js
@@ -13,6 +13,9 @@ function App() {
     const errorText = "Coś poszło nie tak. \n Proszę spróbować później.";
     const { isLoading, data, errorInfo } = useFetchData(dataUrl, errorText);
 
+    const currencies = Object.keys(data.rates);
+    const ratesValues = currencies.map(currency => data.rates[currency]);
+
     return (
         // Homework from the module 8 Frontend Developer
         <>
@@ -32,6 +35,37 @@ function App() {
                 designer="Projektant: Rafał Chudy"
                 copyright="Wszelkie prawa zastrzeżone &copy; 2022"
             />
+
+            {data ?
+                <ul>
+                    {Object.keys(data.rates).map((dataItem, index) => {
+                        <li key={dataItem}>
+                            {dataItem}:
+                            {Object.keys(data.rates).map(
+                                dataItem => data.rates[dataItem]
+                            )}
+                        </li>
+                    })}
+                </ul>
+                : null
+            }
+
+            {data ?
+                <ul>
+                    {currencies.map((currency, index) => {
+                        return <li key={currency}>
+                            {currency}: {ratesValues[index]}
+                        </li>
+                    })}
+                </ul>
+                : null
+            }
+
+            <ul>
+                {Object.keys(data).map((dataItem) => {
+                    return <li key={dataItem}>{dataItem}{data[dataItem]}</li>
+                })}
+            </ul>
         </>
     );
 }
